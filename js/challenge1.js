3f97be32dc25573a630b5369c6cfb146a4e92bef
vjruizumich <vjruiz@umich.edu>
2024-04-09 13:47:22 -0400

    adding files

---
+++ js/challenge1.js
@@ -0,0 +1,9 @@
+var heading;
+window.addEventListener("load", funciton () {
+    heading = document.getElementsByTagName("h1");
+    heading.classList.add("heading");
+
+})
+const h1 = document.getElementsByName("h1");
+h1.classList.add("heading");
+document.onload("");
